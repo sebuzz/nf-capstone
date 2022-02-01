cda9a0d81a4df6a8461c52e4aae62452139e99f3
Sebastian Künne <sebuzz@mac.com>
2022-02-01 18:44:36 +0100

    feat: first card component for testing

---
+++ src/pages/flashcard.jsx
@@ -0,0 +1,16 @@
+
+import React from "react";
+import Link from "next/link";
+
+const Page = () => {
+
+
+	return (
+		<div>
+			Currywurst
+			<Link href="/foobar">Foobar</Link>
+		</div>
+	);
+};
+
+export default Page;
