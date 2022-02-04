b992f67090602fd069f2a37097e005a8b95a5136
Sebastian Künne <sebuzz@mac.com>
2022-02-04 14:29:51 +0100

    feat: filter the lesson data and write to filteredData

--- src/ions/store/store.jsx
+++ src/ions/store/store.jsx
@@ -1,17 +1,42 @@
 import create from "zustand";
 import axios from "axios";
+import produce from "immer";
 
 const useStore = create(set => ({
-	lessonVocabulary: [],
+	lessonData: [],
+	filteredData: [],
+	filtered: false,
+	toggleFiltered: () => {
+		set(
+			produce(state => {
+				state.filtered = !state.filtered;
+			})
+		);
+	},
+	setFilter: type => {
+		set(state => {
+			const clone = [...state.lessonData];
+			const kanjiFilter = clone.filter(word => word.kanji.length > 0);
+			if (type === "kanji") {
+				return {
+					filteredData: kanjiFilter,
+				};
+			}
+		});
+	},
+
 	currentCard: {},
+	// fetch the lesson data and pick the selected card
+	// TODO: separate fetch and card picking so that the fetch only happens once
 	setCurrentCard: async cardNumber => {
 		console.log("attempting to fetch");
 		const response = await axios.get("/vocabulary/japanese/9.json");
 		const result = response.data;
 		const lessonData = result.vocabulary;
+
 		const cardData = lessonData[cardNumber];
 
-		set(() => ({ currentCard: cardData }));
+		set(() => ({ currentCard: cardData, lessonData: lessonData }));
 	},
 }));
 
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,13 +8,29 @@ import randomCardNumber from "../ions/utils/randomCardNumber";
 const Page = () => {
 	const setCurrentCard = useStore(state => state.setCurrentCard);
 	const currentCard = useStore(state => state.currentCard);
+	const lessonData = useStore(state => state.lessonData);
+	const filteredData = useStore(state => state.filteredData);
+	const setFilter = useStore(state => state.setFilter);
+	const toggleFiltered = useStore(state => state.toggleFiltered);
+	const filtered = useStore(state => state.filtered);
 
 	useEffect(() => {
-		setCurrentCard(randomCardNumber(48));
-	}, []);
+		setCurrentCard(randomCardNumber(lessonData.length));
+	}, [setCurrentCard, lessonData.length]);
 
 	return (
 		<Layout>
+			<button
+				type={"button"}
+				onClick={() => {
+					toggleFiltered();
+					setFilter("kanji");
+					console.log(filteredData);
+					console.log("filtered:", filtered);
+				}}
+			>
+				Kanji
+			</button>
 			<Head>
 				<title key="title">KOTOBA</title>
 				<meta key="description" name="description" content="This is my capstone project" />
