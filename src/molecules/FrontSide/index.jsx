a78a7900b06ae2b3cf77fb16dee44d47c9de4cf4
Sebus <sebuzz@mac.com>
2022-02-15 17:35:48 +0100

    feat: added speech synthesis to kana and kanji (#33)

--- src/molecules/FrontSide/index.jsx
+++ src/molecules/FrontSide/index.jsx
@@ -5,22 +5,51 @@ import * as React from "react";
 import useStore from "../../ions/store/store";
 import { StyledCardMainArea } from "./styled";
 
+import { Link } from "@mui/material";
+//import Speech from "react-speech";
+
 const FrontSide = () => {
 	const currentCard = useStore(state => state.currentCard);
 	const showKana = useStore(store => store.showKana);
 	const showTranslation = useStore(store => store.showTranslation);
 	const learnMode = useStore(state => state.learnMode);
 
+	const speakIt = textToSpeak => {
+		let voices = speechSynthesis.getVoices();
+		let utterance = new SpeechSynthesisUtterance(textToSpeak);
+		utterance.voice = voices[58]; // 18 58
+		utterance.lang = "ja-JP";
+		speechSynthesis.speak(utterance);
+	};
+
 	return (
 		<StyledCardMainArea>
 			<Stack justifyContent="center" alignItems="center" spacing={2}>
 				<CardContent>
 					<Typography gutterBottom align="center" variant="h2" component="div">
-						{currentCard.kanji}
+						<Link
+							color="#616161"
+							underline="none"
+							unselectable="on"
+							onClick={() => {
+								speakIt(currentCard.kanji);
+							}}
+						>
+							{currentCard.kanji}
+						</Link>
 					</Typography>
 					{showKana && (
 						<Typography align="center" variant="h4" component="div">
-							{currentCard.word}
+							<Link
+								color="#616161"
+								underline="none"
+								unselectable="on"
+								onClick={() => {
+									speakIt(currentCard.word);
+								}}
+							>
+								{currentCard.word}
+							</Link>
 						</Typography>
 					)}
 					{learnMode && showTranslation && (
@@ -28,6 +57,13 @@ const FrontSide = () => {
 							{currentCard.meaning}
 						</Typography>
 					)}
+					{/*<button*/}
+					{/*	onClick={() => {*/}
+					{/*		speakIt();*/}
+					{/*	}}*/}
+					{/*>*/}
+					{/*	speak*/}
+					{/*</button>*/}
 				</CardContent>
 			</Stack>
 		</StyledCardMainArea>
