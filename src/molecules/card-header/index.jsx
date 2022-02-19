4d5bc132e68abe5996ea1d889075aa130071cb7a
Sebastian Künne <sebuzz@mac.com>
2022-02-19 22:54:03 +0100

    feat: positioned card background elements correctly

--- src/molecules/card-header/index.jsx
+++ src/molecules/card-header/index.jsx
@@ -1,48 +1,42 @@
 import * as React from "react";
 import useStore from "../../ions/store/store";
-import VocabularyNoHolder from "../../atoms/vocabulary-no-holder";
+
 import FilteredHolder from "../../atoms/filtered-holder";
 import TranslationHolder from "../../atoms/translation-holder";
-import MenuDrawer from "../menu-drawer";
+
 import Image from "next/image";
 import styled from "@emotion/styled";
 
 const StyledHeader = styled.div`
 	display: grid;
 	position: absolute;
-	align-items: self-end;
 	z-index: -1;
+	align-items: self-end;
 `;
 
 const StyledLogoWrapper = styled.div`
-	position: relative;
-	top: -55px;
-	left: 10px;
-	opacity: 30%;
+	position: absolute;
+	top: -165px;
+	left: -180px;
+	width: 124px;
+	opacity: 20%;
 `;
 
 const StyledBGWrapper = styled.div`
-	position: relative;
-	top: 100px;
-	left: 0px;
-	opacity: 40%;
+	position: absolute;
+	top: 80px;
+	left: -220px;
+	width: 445px;
+	opacity: 45%;
 `;
 
 const CardHeader = () => {
-	const currentCard = useStore(state => state.currentCard);
 	const showKana = useStore(state => state.showKana);
 	const showTranslation = useStore(state => state.showTranslation);
-	//const shownCards = useStore(state => state.shownCards);
-
-	//const cardNumber = currentCard.vocabularyNo;
-	//const thisCard = shownCards.find(element => element.cardNumber === cardNumber);
-	//const timesShown = thisCard ? thisCard.occurrenceSC : 0;
 	const debugMode = useStore(store => store.debugMode);
 
 	return (
 		<StyledHeader>
-			{/*<Image src="/images/Logo_Header_@2x.png" width={445} height={164} alt="header title" />*/}
-			{/*<img href="/images/Logo_full.svg" alt="logo" />*/}
 			<StyledLogoWrapper>
 				<Image
 					src="/images/Logo_full_05@05.png"
@@ -63,7 +57,6 @@ const CardHeader = () => {
 					</div>
 				)}
 			</div>
-			{/*<MenuDrawer />*/}
 		</StyledHeader>
 	);
 };
