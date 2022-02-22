e5cd224f0e4cb398ab1aa2ee6c6aada1d07ca492
Sebastian Künne <sebuzz@mac.com>
2022-02-23 00:00:04 +0100

    feat: chapter selection page styling tweaks

--- src/molecules/ChapterSelector/styled.js
+++ src/molecules/ChapterSelector/styled.js
@@ -8,18 +8,21 @@ export const StyledChapterSelector = styled.div`
 	max-width: ${pxToRem(50)};
 	height: ${pxToRem(50)};
 	max-height: ${pxToRem(50)};
+	padding-left: 0.15em;
 	border: none;
 	border-radius: ${pxToRem(4)};
 	background: hsl(${props => props.ballColor}, 90%, 50%);
+	box-shadow: -5px -5px 15px hsla(${props => props.ballColor}, 90%, 50%, 0.3);
 	color: #000;
 	font-size: 1em;
 
 	&:hover {
-		background: #d8b;
+		background: #364996;
+		color: #e2e2e2;
 	}
 
 	&:active {
-		background: #bad;
+		background: #dcb;
 	}
 `;
 
@@ -31,8 +34,17 @@ export const StyledChapterSelectorWrapper = styled.div`
 	height: ${pxToRem(50)};
 	max-height: ${pxToRem(50)};
 	margin: ${pxToRem(10)};
-	border: 1px solid rgba(26, 39, 86, 1);
 	border-radius: ${pxToRem(4)};
-	background: rgba(26, 39, 86, 1);
-	box-shadow: 12px 12px 12px 1px rgba(0, 0, 5, 0.5);
+	background: linear-gradient(
+		142deg,
+		rgba(0, 0, 0, 0.6601015406162465) 0%,
+		rgba(145, 126, 87, 1) 0%,
+		rgba(103, 90, 61, 1) 100%
+	);
+	box-shadow: 2px 2px 5px rgba(0, 0, 5, 0.9), 12px 12px 12px 1px rgba(0, 0, 5, 0.5);
+
+	&:hover {
+		background: #364996;
+		color: #e2e2e2;
+	}
 `;
